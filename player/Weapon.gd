abfaaa9eca2ef799900ac566719ade9a5de99a48
ReddXAce <anthony.ruiz4@upr.edu>
2023-07-09 02:26:28 -0400

    Added shooting (not really working rn)

--- player/Weapon.gd
+++ player/Weapon.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var bullet_speed = 1000
+var bullet = preload("res://weapons/scenes/Bullet1.tscn")
+
 func _process(_delta):
 	look_at(get_global_mouse_position())
 	if rotation_degrees > 90 or rotation_degrees < -90:
@@ -11,6 +14,15 @@ func _process(_delta):
 
 	if rotation_degrees >= 269:
 		rotation_degrees = 90
-
 	if rotation_degrees <= -269:
 		rotation_degrees = -90
+
+	if Input.is_action_just_pressed("Shoot"):
+		_fire()
+
+func _fire(): #Maybe change parameters for different bullet types?
+	var bullet_instance = bullet.instantiate()
+	bullet_instance.position = get_global_position()
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(global_rotation))
+	get_tree().get_root().add_child(bullet_instance)#call_deferred("add_child", bullet_instance)
