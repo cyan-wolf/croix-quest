4e8ca39fe6b716261de894c9efb9f14eb4bb3243
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-07-09 16:28:29 -0400

    Fixed player shooting (using MATH)

--- player/Weapon.gd
+++ player/Weapon.gd
@@ -1,28 +1,33 @@
 extends Node2D
 
-var bullet_speed = 1000
-var bullet = preload("res://weapons/scenes/Bullet1.tscn")
+var _bullet_speed: float = 1000.0
+var _bullet_scene := preload("res://weapons/scenes/Bullet1.tscn")
 
 func _process(_delta):
-	look_at(get_global_mouse_position())
-	if rotation_degrees > 90 or rotation_degrees < -90:
+	self.look_at(self.get_global_mouse_position())
+
+	if self.rotation_degrees > 90 or self.rotation_degrees < -90:
 		$Sprite2D.flip_v = true
 	else:
 		$Sprite2D.flip_v = false
 
-	
-
-	if rotation_degrees >= 269:
-		rotation_degrees = 90
-	if rotation_degrees <= -269:
-		rotation_degrees = -90
+	if self.rotation_degrees >= 269:
+		self.rotation_degrees = 90
+	if self.rotation_degrees <= -269:
+		self.rotation_degrees = -90
 
 	if Input.is_action_just_pressed("Shoot"):
 		_fire()
 
-func _fire(): #Maybe change parameters for different bullet types?
-	var bullet_instance = bullet.instantiate()
-	bullet_instance.position = get_global_position()
-	bullet_instance.rotation_degrees = rotation_degrees
-	bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(global_rotation))
-	get_tree().get_root().add_child(bullet_instance)#call_deferred("add_child", bullet_instance)
+
+func _fire(): #Maybe change parameters for different _bullet_scene types?
+	var bullet_instance := _bullet_scene.instantiate()
+	bullet_instance.position = self.global_position
+	bullet_instance.rotation = self.rotation
+
+	# Calculated using math.
+	var impulse := Vector2(cos(self.rotation), sin(self.rotation)) * _bullet_speed
+
+	bullet_instance.apply_impulse(impulse)
+	self.get_tree().get_root().add_child(bullet_instance)#call_deferred("add_child", bullet_instance)
+
