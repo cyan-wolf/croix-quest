2c33b61f6a04799e88dfd7c22ddfca572372b58a
ReddXAce <anthony.ruiz4@upr.edu>
2023-07-10 01:30:16 -0400

    Made a system for multiple weapons + shotgun code

--- player/Weapon.gd
+++ player/Weapon.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var weapon_type := "Gun"
 var _bullet_speed: float = 1000.0
 var _bullet_scene := preload("res://weapons/scenes/Bullet1.tscn")
 
@@ -17,17 +18,29 @@ func _process(_delta):
 		self.rotation_degrees = -90
 
 	if Input.is_action_just_pressed("Shoot"):
-		_fire()
+		_fire(weapon_type)
 
 
-func _fire(): #Maybe change parameters for different _bullet_scene types?
-	var bullet_instance := _bullet_scene.instantiate()
-	bullet_instance.position = self.global_position
-	bullet_instance.rotation = self.rotation
+func _fire(weapon_type): #Maybe change parameters for different _bullet_scene types?
+	match weapon_type: #BASICALLY the switch statement, just add "[weapon_type]": to add a case
+		"Gun":
+			var bullet_instance := _bullet_scene.instantiate()
+			bullet_instance.position = self.global_position
+			bullet_instance.rotation = self.rotation
 
-	# Calculated using math.
-	var impulse := Vector2(cos(self.rotation), sin(self.rotation)) * _bullet_speed
+			# Calculated using math.
+			var impulse := Vector2(cos(self.rotation), sin(self.rotation)) * _bullet_speed
 
-	bullet_instance.apply_impulse(impulse)
-	self.get_tree().get_root().add_child(bullet_instance)#call_deferred("add_child", bullet_instance)
+			bullet_instance.apply_impulse(impulse)
+			self.get_tree().get_root().add_child(bullet_instance)
+		"Shotgun":
+			for i in range(3):
+				var bullet_instance = _bullet_scene.instantiate()
+				bullet_instance.position = self.global_position
+				bullet_instance.rotation = self.rotation + (i - 1) * 0.1
+		
+				var impulse = Vector2(cos(bullet_instance.rotation), sin(bullet_instance.rotation)) * _bullet_speed
+				bullet_instance.apply_impulse(impulse)
+		
+				self.get_tree().get_root().add_child(bullet_instance)
 
