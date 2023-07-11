a4d6f3c07a11a21887d167280880e8eadaf87417
ReddXAce <anthony.ruiz4@upr.edu>
2023-07-11 00:36:02 -0400

    Added Sprites + Sniper + Bullet Variables

--- player/Weapon.gd
+++ player/Weapon.gd
@@ -3,6 +3,9 @@ extends Node2D
 var weapon_type := "Shotgun"
 var _bullet_speed: float = 1000.0
 var _bullet_scene := preload("res://weapons/scenes/Bullet1.tscn")
+var bullet_alive_time: float = 0.0
+var damage = 0
+
 @onready var sprite = $Sprite2D
 
 func _process(_delta):
@@ -21,19 +24,36 @@ func _process(_delta):
 	if Input.is_action_just_pressed("Shoot"):
 		_fire(weapon_type)
 
-	if Input.is_action_just_pressed("Test"): #Switches weapons. TODO: Make another input that switches backwards
+	if Input.is_action_just_pressed("Test"): #Switches weapons. TODO: Make another input that switches backwards and flags for picking them up
 		if weapon_type == "Shotgun":
 			weapon_type = "Gun"
-		else:
+		elif weapon_type == "Gun":
+			weapon_type = "Sniper"
+		elif weapon_type == "Sniper":
 			weapon_type = "Shotgun"
 
 	match weapon_type: #change texture of weapon depending on the weapon
 		"Gun":
 			sprite.texture = load("res://weapons/sprites/Pistol/pistol.png")
-			sprite.scale = Vector2(0.3, 0.3)
+			sprite.scale = Vector2(0.5, 0.5)
+			_bullet_speed = 1000.0
+			bullet_alive_time = 0.5
+			damage = 2
 		"Shotgun":
 			sprite.texture = load("res://weapons/sprites/Shotgun/Shotgun.png")
 			sprite.scale = Vector2(0.5, 0.5)
+			_bullet_speed = 500
+			bullet_alive_time = 0.25
+			damage = 3
+		"Sniper":
+			sprite.texture = load("res://weapons/sprites/Sniper/Sniper.png")
+			sprite.scale = Vector2(0.5, 0.5)
+			_bullet_speed = 2000
+			bullet_alive_time = 5
+			damage = 10
+
+func _remove_bullet(bullet):
+	bullet.queue_free()
 
 
 func _fire(weapon_type): #Maybe change parameters for different _bullet_scene types?
@@ -42,6 +62,8 @@ func _fire(weapon_type): #Maybe change parameters for different _bullet_scene ty
 			var bullet_instance := _bullet_scene.instantiate()
 			bullet_instance.position = self.global_position
 			bullet_instance.rotation = self.rotation
+			bullet_instance.lifetime = bullet_alive_time
+			bullet_instance.damage = damage
 
 			# Calculated using math.
 			var impulse := Vector2(cos(self.rotation), sin(self.rotation)) * _bullet_speed
@@ -53,9 +75,22 @@ func _fire(weapon_type): #Maybe change parameters for different _bullet_scene ty
 				var bullet_instance = _bullet_scene.instantiate()
 				bullet_instance.position = self.global_position
 				bullet_instance.rotation = self.rotation + (i - 1) * 0.1
-		
+				bullet_instance.lifetime = bullet_alive_time
+				bullet_instance.damage = damage
+
 				var impulse = Vector2(cos(bullet_instance.rotation), sin(bullet_instance.rotation)) * _bullet_speed
 				bullet_instance.apply_impulse(impulse)
 		
-				self.get_tree().get_root().add_child(bullet_instance)
+				self.get_tree().get_root().add_child(bullet_instance)	
+		"Sniper":
+			var bullet_instance := _bullet_scene.instantiate()
+			bullet_instance.position = self.global_position
+			bullet_instance.rotation = self.rotation
+			bullet_instance.lifetime = bullet_alive_time
+			bullet_instance.damage = damage
 
+			# Calculated using math.
+			var impulse := Vector2(cos(self.rotation), sin(self.rotation)) * _bullet_speed
+
+			bullet_instance.apply_impulse(impulse)
+			self.get_tree().get_root().add_child(bullet_instance)
---
+++ weapons/scripts/Bullet1.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+var lifetime: float = 0.0
+var damage = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Despawn the bullet if it doesn't hit anything by this point.
+	await self.get_tree().create_timer(lifetime).timeout
+	self.queue_free()
+	#print_debug(damage)
+
+
+# # Called every frame. 'delta' is the elapsed time since the previous frame.
+# func _process(delta):
+# 	pass
