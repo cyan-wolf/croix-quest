616550960b675b8dffe486c82f587cbc7391bbb1
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-07-15 07:23:21 -0400

    Started working on enemy AI

---
+++ enemies/melee_enemy/melee_enemy.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@onready var _nav_agent: NavigationAgent2D = self.get_node("NavigationAgent2D")
+
+func _ready() -> void:
+	# TODO: add enemy AI
+
+	pass
+
+
+func _physics_process(_delta: float) -> void:
+	# TODO: add enemy AI
+
+	pass
+
+
