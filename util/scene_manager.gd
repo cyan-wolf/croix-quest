38eb1c97fd133db8cac8b3cc2ecbd4d30917797b
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-07-21 11:27:02 -0400

    Added some utility functions

--- util/scene_manager.gd
+++ util/scene_manager.gd
@@ -7,3 +7,11 @@ func load_scene_file(scene_path: String) -> void:
 func load_packed_scene(scene: PackedScene) -> void:
 	self.get_tree().change_scene_to_packed(scene)
 
+
+func find_player() -> Player:
+	return self.get_tree().current_scene.get_node("Player") as Player
+
+
+func find_camera() -> Camera2D:
+	return self.get_viewport().get_camera_2d()
+
