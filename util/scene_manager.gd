21be001307a159e20133af55dae4a8596542f497
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-07-25 10:49:58 -0400

    Added loading screen

--- util/scene_manager.gd
+++ util/scene_manager.gd
@@ -1,11 +1,25 @@
 extends Node2D
 
+@onready var _loading_screen: Control = self.get_node("CanvasLayer/LoadingScreen")
+
 func load_scene_file(scene_path: String) -> void:
+	self.show_loading_screen()
 	self.get_tree().change_scene_to_file(scene_path)
+
+	# Show the loading screen while the scene is loading.
+	# Hide it again after the scene has loaded.
+	var callback: Callable = (func():
+		await self.get_tree().tree_changed
+		await self.async_delay(0.5)
+		
+		self.hide_loading_screen()
+	)
+
+	callback.call()
 
 
 func load_packed_scene(scene: PackedScene) -> void:
-	self.get_tree().change_scene_to_packed(scene)
+	self.load_scene_file(scene.resource_path)
 
 
 func find_player() -> Player:
@@ -19,4 +33,11 @@ func find_camera() -> Camera2D:
 func async_delay(delay_in_secs: float) -> void:
 	await self.get_tree().create_timer(delay_in_secs).timeout
 
+
+func show_loading_screen() -> void:
+	_loading_screen.show()
+
+
+func hide_loading_screen() -> void:
+	_loading_screen.hide()
 
--- util/teleporter/teleporter.gd
+++ util/teleporter/teleporter.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var _hitbox: Area2D = self.get_node("HitboxArea")
 
 @export var _teleport_destination: Vector2
+@export var _show_loading_screen: bool = true
 
 func _ready() -> void:
 	_hitbox.area_entered.connect(_on_area_entered_hitbox)
@@ -10,6 +11,12 @@ func _ready() -> void:
 
 func _on_area_entered_hitbox(other_hitbox: Area2D) -> void:
 	if other_hitbox.is_in_group("player_hitbox"):
+		SceneManager.show_loading_screen()
+		await SceneManager.async_delay(0.5)
+
 		SceneManager.find_player().global_position = _teleport_destination
+
+		await SceneManager.async_delay(0.5)
+		SceneManager.hide_loading_screen()
 
 
