42b33c366611f970993a91511df4e156105468f7
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-07-29 12:15:12 -0400

    Updated how the teleporter works

--- util/teleporter/teleporter.gd
+++ util/teleporter/teleporter.gd
@@ -1,22 +1,29 @@
 extends Node2D
 
 @onready var _hitbox: Area2D = self.get_node("HitboxArea")
+# The teleporter should have a 'TeleporterDestination' child node (a separate one in each scene).
+@onready var _teleporter_destination: Node2D = self.get_node("TeleporterDestination")
 
-@export var _teleport_destination: Vector2
 @export var _show_loading_screen: bool = true
 
 func _ready() -> void:
 	_hitbox.area_entered.connect(_on_area_entered_hitbox)
 
+	# Hide the teleporter's sprites since they are just markers.
+	self.hide()
+	_teleporter_destination.hide()
+
 
 func _on_area_entered_hitbox(other_hitbox: Area2D) -> void:
 	if other_hitbox.is_in_group("player_hitbox"):
-		SceneManager.show_loading_screen()
-		await SceneManager.async_delay(0.5)
+		if _show_loading_screen:
+			SceneManager.show_loading_screen()
+			await SceneManager.async_delay(0.5)
 
-		SceneManager.find_player().global_position = _teleport_destination
+		SceneManager.find_player().global_position = _teleporter_destination.global_position
 
-		await SceneManager.async_delay(0.5)
-		SceneManager.hide_loading_screen()
+		if _show_loading_screen:
+			await SceneManager.async_delay(0.5)
+			SceneManager.hide_loading_screen()
 
 
