c755f8267b461fd387ec6ea69b5a2a600e951f0e
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-08-09 10:04:59 -0400

    Finished `InteractionComponent`

--- util/components/interaction/interaction_component.gd
+++ util/components/interaction/interaction_component.gd
@@ -1,20 +1,41 @@
 extends Node2D
 
-# TODO: 
+signal interact
+
 # This node should be added as a child node on other scenes.
 # This node should send a signal when the player tries to "interact" with it,
 # and it should only send out this signal if the player is within range.
-# The range can be determined using the hitbox.
 
 # This node does not come with the scene, it should be added manually.
 @onready var _hitbox: Area2D = self.get_node("HitboxArea")
 
+# Maximum distance that the player needs to be within before 
+# this component can be interacted with (in pixels).
+@export var _max_interaction_distance: float = 32.0
+
+var _mouse_is_in_hitbox := false
+
 func _ready() -> void:
-	_hitbox.area_entered.connect(_on_area_entered_hitbox)
+	_hitbox.mouse_entered.connect(_on_mouse_entered_hitbox)
+	_hitbox.mouse_exited.connect(_on_mouse_exited_hitbox)
 
 
-# This method might not be needed.
-func _on_area_entered_hitbox(other_hitbox: Area2D) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact") \
+		and _mouse_is_in_hitbox \
+		and _get_distance_to_player() < _max_interaction_distance:
+		
+		self.interact.emit()
 
+
+func _on_mouse_entered_hitbox() -> void:
+	_mouse_is_in_hitbox = true
+
+
+func _on_mouse_exited_hitbox() -> void:
+	_mouse_is_in_hitbox = false
+
+
+func _get_distance_to_player() -> float:
+	return self.global_position.distance_to(SceneManager.find_player().global_position)
 
