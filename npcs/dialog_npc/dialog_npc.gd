e51d8f5ce6f9e2548d0dd7c289960ffb4a4f27fe
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-08-13 10:56:28 -0400

    Finished `DialogNPC`s

--- npcs/dialog_npc/dialog_npc.gd
+++ npcs/dialog_npc/dialog_npc.gd
@@ -1,34 +1,15 @@
 extends CharacterBody2D
 
-@onready var _hitbox: Area2D = self.get_node("HitboxArea")
+@onready var _interaction_component: InteractionComponent = self.get_node("InteractionComponent")
 
 @export var _animated_sprite: AnimatedSprite2D
 @export var _dialog: DialogResource
 
-var _can_be_interacted_with := false
-
 func _ready() -> void:
-	self.input_event.connect(_on_input_event)
-	_hitbox.area_entered.connect(_on_area_entered_hitbox)
-	_hitbox.area_exited.connect(_on_area_exited_hitbox)
-
+	_interaction_component.interact.connect(_on_player_interact)
 	_animated_sprite.play("idle")
 
 
-# FIXME: Figure out why this node doesn't register mouse clicks.
-# (this node should start a dialog if clicked on by the mouse when the player is nearby)
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_RIGHT:
-		if _can_be_interacted_with:
-			DialogManager.start_dialog(_dialog)
-
-
-func _on_area_entered_hitbox(other_hitbox: Area2D) -> void:
-	if other_hitbox.is_in_group("player_hitbox"):
-		_can_be_interacted_with = true
-
-
-func _on_area_exited_hitbox(other_hitbox: Area2D) -> void:
-	if other_hitbox.is_in_group("player_hitbox"):
-		_can_be_interacted_with = false
+func _on_player_interact() -> void:
+	DialogManager.start_dialog(_dialog)
 
