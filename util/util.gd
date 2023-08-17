eea2a4fd23d24afeb6bd9b471816dfd3184112a9
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-08-17 09:48:47 -0400

    Added array utility function

--- util/util.gd
+++ util/util.gd
@@ -7,3 +7,16 @@ enum Direction {
 	LEFT = 1,
 }
 
+## Returns an array of size `amount` containing unique random elements from `array`.
+static func choose_random_elements_from_array(array: Array, amount: int) -> Array:
+	var copied_array := array.duplicate() # shallow copy
+
+	randomize()
+	# Randomizes the order of a copy of the array.
+	# Example: [1, 2, 3, 4] -> [2, 3, 1, 4]
+	copied_array.shuffle()
+
+	# Returns the first elements given by `amount`.
+	# Example: If `amount` = 2, this function would return [2, 3]. 
+	return copied_array.slice(0, amount)
+
