dc08ffb5f8d8d8b28655db5122e94f6391d0c4ba
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-10-02 09:27:07 -0400

    Worked on Tauron boss

--- enemies/bosses/tauron/tauron_boss.gd
+++ enemies/bosses/tauron/tauron_boss.gd
@@ -2,13 +2,33 @@ extends CharacterBody2D
 
 const Projectile := preload("res://weapons/projectile/projectile.gd")
 
+enum AttackState {
+	NONE,
+	STOMP,
+	PROJECTILE,
+	CHARGE,
+}
+
 signal perform_attack_1
 signal perform_attack_2
 signal perform_attack_3
 
 @export var health_component: HealthComponent
 
+## How much damage the stomp attack does.
+@export var _stomp_attack_damage: int = 1
+
+## How much damage the projectile attack does.
+@export var _projectile_attack_damage: int = 1
+
+## How much damage the charge (tackle) attack does.
+@export var _charge_attack_damage: int = 1
+
 @onready var _hitbox: Area2D = self.get_node("HitboxArea")
+@onready var _stomp_attack_hitbox: Area2D = self.get_node("StompAttackHitboxArea")
+@onready var _charge_attack_hitbox: Area2D = self.get_node("ChargeAttackHitboxArea")
+
+var _current_attack_state := AttackState.NONE
 
 var _has_been_defeated := false
 
@@ -16,6 +36,10 @@ func _ready() -> void:
 	# Connect the necessary signals.
 	_hitbox.area_entered.connect(_on_area_entered_hitbox)
 	self.health_component.death.connect(_on_death)
+
+	($StompAttackTimer as Timer).timeout.connect(_on_stomp_attack_timer_timeout)
+	($ProjectileAttackTimer as Timer).timeout.connect(_on_projectile_attack_timer_timeout)
+	($ChargeAttackTimer as Timer).timeout.connect(_on_charge_attack_timer_timeout)
 
 	self.perform_attack_1.connect(_async_on_perform_attack_1)
 	self.perform_attack_2.connect(_async_on_perform_attack_2)
@@ -39,14 +63,15 @@ func _async_on_perform_attack_1() -> void:
 	print_debug("DEBUG: In phase 1")
 	await SceneManager.async_delay(4.0)
 
-	var jump_height := 20.0 * 16
+	var jump_height := 20.0 * 16	# in pixels
+	var jump_duration := 0.3		# in seconds
 
 	# Wait for the boss to jump.
 	await self.get_tree().create_tween().tween_property(
 		self, 
 		"global_position", 
 		self.global_position + Vector2.UP * jump_height,
-		0.2,
+		jump_duration,
 	).finished
 
 	await SceneManager.async_delay(2.0)
@@ -58,15 +83,25 @@ func _async_on_perform_attack_1() -> void:
 
 	await SceneManager.async_delay(0.5)
 
+	# This activates the stomp attack hitbox.
+	_current_attack_state = AttackState.STOMP
+
+	# TODO: Add a screen shake SFX when landing.
+
 	# Wait for the boss to land where the player was.
 	await self.get_tree().create_tween().tween_property(
 		self, 
 		"global_position", 
 		player_pos,
-		0.2,
+		jump_duration,
 	).finished
 
-	await SceneManager.async_delay(4.0)
+	await SceneManager.async_delay(2.0)
+
+	# This deactivates the stomp attack hitbox.
+	_current_attack_state = AttackState.NONE
+
+	await SceneManager.async_delay(2.0)
 
 	if _has_been_defeated:
 		# Async call (no need to wait for the cutscene to finish).
@@ -114,3 +149,31 @@ func _async_play_defeated_cutscene() -> void:
 func _on_death() -> void:
 	_has_been_defeated = true
 
+
+func _on_stomp_attack_timer_timeout() -> void:
+	if _current_attack_state != AttackState.STOMP:
+		return
+
+	var stomp_attack_collison: CollisionShape2D = _stomp_attack_hitbox.get_node("CollisionShape2D")
+
+	# Enables and disables the stomp attack each time the timer emits its `timeout` signal.
+	stomp_attack_collison.set_deferred("disabled", not stomp_attack_collison.disabled)
+
+
+# NOTE: This method might not be necessary, the projectiles could 
+# just be fired in a loop.
+func _on_projectile_attack_timer_timeout() -> void:
+	if _current_attack_state != AttackState.PROJECTILE:
+		return
+
+	# TODO: Shoot projectiles.
+
+
+func _on_charge_attack_timer_timeout() -> void:
+	if _current_attack_state != AttackState.CHARGE:
+		return
+
+	var charge_attack_collison: CollisionShape2D = _charge_attack_hitbox.get_node("CollisionShape2D")
+
+	# Enables and disables the stomp attack each time the timer emits its `timeout` signal.
+	charge_attack_collison.set_deferred("disabled", not charge_attack_collison.disabled)
