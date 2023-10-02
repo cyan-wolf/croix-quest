84c092a6d57b7c2decf4decb5f6ac02fe16c48b9
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-10-02 08:53:32 -0400

    Started working on Tauron boss stomp attack

--- enemies/bosses/tauron/tauron_boss.gd
+++ enemies/bosses/tauron/tauron_boss.gd
@@ -21,6 +21,9 @@ func _ready() -> void:
 	self.perform_attack_2.connect(_async_on_perform_attack_2)
 	self.perform_attack_3.connect(_async_on_perform_attack_3)
 
+	# Start the cycle of attacks.
+	self.perform_attack_1.emit()
+
 
 func _on_area_entered_hitbox(other_hitbox: Area2D) -> void:
 	# Logic for taking projectile damage.
@@ -33,6 +36,37 @@ func _on_area_entered_hitbox(other_hitbox: Area2D) -> void:
 # A stomping attack.
 func _async_on_perform_attack_1() -> void:
 	# TODO: Attack logic goes here.
+	print_debug("DEBUG: In phase 1")
+	await SceneManager.async_delay(4.0)
+
+	var jump_height := 20.0 * 16
+
+	# Wait for the boss to jump.
+	await self.get_tree().create_tween().tween_property(
+		self, 
+		"global_position", 
+		self.global_position + Vector2.UP * jump_height,
+		0.2,
+	).finished
+
+	await SceneManager.async_delay(2.0)
+
+	# Player position at the current moment.
+	var player_pos: Vector2 = SceneManager.find_player().global_position
+
+	self.global_position = player_pos + Vector2.UP * jump_height
+
+	await SceneManager.async_delay(0.5)
+
+	# Wait for the boss to land where the player was.
+	await self.get_tree().create_tween().tween_property(
+		self, 
+		"global_position", 
+		player_pos,
+		0.2,
+	).finished
+
+	await SceneManager.async_delay(4.0)
 
 	if _has_been_defeated:
 		# Async call (no need to wait for the cutscene to finish).
@@ -45,6 +79,8 @@ func _async_on_perform_attack_1() -> void:
 # A projectile attack.
 func _async_on_perform_attack_2() -> void:
 	# TODO: Attack logic goes here.
+	print_debug("DEBUG: In phase 2")
+	await SceneManager.async_delay(4.0)
 
 	if _has_been_defeated:
 		# Async call (no need to wait for the cutscene to finish).
@@ -57,6 +93,8 @@ func _async_on_perform_attack_2() -> void:
 # A charging attack.
 func _async_on_perform_attack_3() -> void:
 	# TODO: Attack logic goes here.
+	print_debug("DEBUG: In phase 3")
+	await SceneManager.async_delay(4.0)
 
 	if _has_been_defeated:
 		# Async call (no need to wait for the cutscene to finish).
@@ -69,6 +107,7 @@ func _async_on_perform_attack_3() -> void:
 # Plays after the boss finishes its current phase and health is 0.
 func _async_play_defeated_cutscene() -> void:
 	# TODO
+	print_debug("DEBUG: Tauron boss has died.")
 	pass
 
 
