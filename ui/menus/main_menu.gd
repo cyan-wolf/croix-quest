f4b68f604e8d8b2e912990abccc7217160e2e57b
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-11-26 10:59:08 -0400

    Fixed weird music bug + fixed weird pause bug in intro castle area

--- ui/menus/main_menu.gd
+++ ui/menus/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
-@export var _new_game_scene: PackedScene
-@export var _settings_menu_scene: PackedScene
+@export_file("*.tscn") var _new_game_scene: String
+@export_file("*.tscn") var _settings_menu_scene: String
 
 @onready var _new_game_button: Button = self.get_node("NewGameButton")
 @onready var _settings_button: Button = self.get_node("SettingsButton")
@@ -10,12 +10,16 @@ func _ready() -> void:
 	_new_game_button.pressed.connect(_on_new_game_button_pressed)
 	_settings_button.pressed.connect(_on_settings_button_pressed)
 
+	SceneManager.play_background_music("res://sounds/music/House In A Forest Loop/House In A Forest Loop.mp3")
+
 
 func _on_new_game_button_pressed() -> void:
-	SceneManager.load_packed_scene(_new_game_scene)
+	SceneManager.stop_playing_background_music()
+	
+	SceneManager.load_scene_file(_new_game_scene)
 
 
 func _on_settings_button_pressed() -> void:
-	SceneManager.load_packed_scene(_settings_menu_scene)
+	SceneManager.load_scene_file(_settings_menu_scene)
 
 
