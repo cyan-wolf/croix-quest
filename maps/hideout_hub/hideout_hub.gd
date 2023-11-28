0ba5cbc9eb590d06000bf3ad3b1bfb4e4e7b6562
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-11-28 09:42:19 -0400

    Spirits now appear in the hub as you rescue them by beating bosses

--- maps/hideout_hub/hideout_hub.gd
+++ maps/hideout_hub/hideout_hub.gd
@@ -13,17 +13,31 @@ func _async_manage_hideout_state_based_on_completed_milestones() -> void:
 		# Configure the scene based on progression in the game.
 		print_debug("Should be in post-Dunkel configuration")
 
+		_put_cat_spirit_in_hub()
+		_put_frog_spirit_in_hub()
+		_put_dog_spirit_in_hub()
+		_put_penguin_spirit_in_hub()
+
 	elif progression_manager.has_milestone(Util.Milestone.VODOROD_DUNGEON_COMPLETED):
 		# Configure the scene based on progression in the game.
 		print_debug("Should be in post-Vodorod configuration")
+
+		_put_cat_spirit_in_hub()
+		_put_frog_spirit_in_hub()
+		_put_dog_spirit_in_hub()
 
 	elif progression_manager.has_milestone(Util.Milestone.ULMUS_DUNGEON_COMPLETED):
 		# Configure the scene based on progression in the game.
 		print_debug("Should be in post-Ulmus configuration")
 
+		_put_cat_spirit_in_hub()
+		_put_frog_spirit_in_hub()
+
 	elif progression_manager.has_milestone(Util.Milestone.COBALT_DUNGEON_COMPLETED):
 		# Configure the scene based on progression in the game.
 		print_debug("Should be in post-Cobalt configuration")
+
+		_put_cat_spirit_in_hub()
 
 	# This one might not be necessary since this could be the same as the default state.
 	elif progression_manager.has_milestone(Util.Milestone.INTRO_CASTLE_COMPLETED):
@@ -75,5 +89,33 @@ func _async_play_return_from_vodorod_cutscene() -> void:
 
 func _async_play_return_from_dunkel_cutscene() -> void:
 	print_debug("Should play the 'Dunkel Dungeon' cutscene")
+
+
+func _put_cat_spirit_in_hub() -> void:
+	var cat_spirit: Node2D = self.get_node("HubElements/ForestSprits/CatDialogNPC")
+	var cat_hub_pos: Node2D = self.get_node("HubElements/ForestSpiritHubPositions/CatPosition")
+
+	cat_spirit.global_position = cat_hub_pos.global_position
+
+
+func _put_frog_spirit_in_hub() -> void:
+	var frog_spirit: Node2D = self.get_node("HubElements/ForestSprits/FrogDialogNPC")
+	var frog_hub_pos: Node2D = self.get_node("HubElements/ForestSpiritHubPositions/FrogPosition")
+
+	frog_spirit.global_position = frog_hub_pos.global_position
+
+
+func _put_dog_spirit_in_hub() -> void:
+	var dog_spirit: Node2D = self.get_node("HubElements/ForestSprits/DogDialogNPC")
+	var dog_hub_pos: Node2D = self.get_node("HubElements/ForestSpiritHubPositions/DogPosition")
+
+	dog_spirit.global_position = dog_hub_pos.global_position
+
+
+func _put_penguin_spirit_in_hub() -> void:
+	var penguin_spirit: Node2D = self.get_node("HubElements/ForestSprits/PenguinDialogNPC")
+	var penguin_hub_pos: Node2D = self.get_node("HubElements/ForestSpiritHubPositions/PenguinPosition")
+
+	penguin_spirit.global_position = penguin_hub_pos.global_position
 
 
