daa2051f5cab051d5f0271fc4c2c4d0dce702ae8
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-11-30 09:32:41 -0400

    Added a really heavy rock

---
+++ interactables/stationary_rock/stationary_rock.gd
@@ -0,0 +1,16 @@
+extends AnimatableBody2D
+
+# This rock cannot be moved, instead a dialog pops up telling the 
+# player the futility of their actions.
+@export var _cant_move_dialog: DialogResource
+
+@onready var _interaction_component: InteractionComponent = self.get_node("InteractionComponent")
+
+func _ready() -> void:
+	_interaction_component.interact.connect(_on_player_interact)
+
+
+func _on_player_interact() -> void:
+	DialogManager.start_dialog(_cant_move_dialog)
+
+
