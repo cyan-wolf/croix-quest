5c64b3e25f63a5fb976f2580ac6969ae4a49cdb1
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-12-12 08:54:52 -0400

    Fixed item interactions bug due to Godot 4.2

--- util/components/interaction/interaction_component.gd
+++ util/components/interaction/interaction_component.gd
@@ -8,35 +8,29 @@ signal interact
 # and it should only send out this signal if the player is within range.
 
 # This node does not come with the scene, it should be added manually.
+# This node is useless as of Godot 4.2.
 @onready var _hitbox: Area2D = self.get_node("HitboxArea")
 
 ## Maximum distance that the player needs to be within before 
 ## this component can be interacted with (in pixels).
 @export var _max_interaction_distance: float = 32.0
 
-var _mouse_is_in_hitbox := false
-
-func _ready() -> void:
-	_hitbox.mouse_entered.connect(_on_mouse_entered_hitbox)
-	_hitbox.mouse_exited.connect(_on_mouse_exited_hitbox)
+## Maximum distance that the mouse needs to be to interact with this component.
+@export var _max_mouse_interaction_distance: float = 8.0
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") \
-		and _mouse_is_in_hitbox \
-		and _get_distance_to_player() < _max_interaction_distance:
-		
+		and _get_distance_to_player() < _max_interaction_distance \
+		and _get_mouse_dist_to_self() < _max_mouse_interaction_distance:
+
 		self.interact.emit()
-
-
-func _on_mouse_entered_hitbox() -> void:
-	_mouse_is_in_hitbox = true
-
-
-func _on_mouse_exited_hitbox() -> void:
-	_mouse_is_in_hitbox = false
 
 
 func _get_distance_to_player() -> float:
 	return self.global_position.distance_to(SceneManager.find_player().global_position)
+
+
+func _get_mouse_dist_to_self() -> float:
+	return self.get_global_mouse_position().distance_to(self.global_position)
 
