7ef028a487e42fd90333a8aed8496acf166e26e2
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-12-26 12:16:49 -0400

    Worked on Dunkel maze

--- maps/dungeons/dunkel_dungeon/lore/dunkel_dungeon_lore_manager.gd
+++ maps/dungeons/dunkel_dungeon/lore/dunkel_dungeon_lore_manager.gd
@@ -18,12 +18,28 @@ func _ready():
 	# Turn off the scene's "light" in the third part of the maze.
 	self.get_node("../Props/TeleportersCollection/Teleporter4") \
 		.used \
-		.connect(func(): _set_dungeon_light_level(SceneLightLevel.PITCH_DARK))
+		.connect(func(): 
+			_set_dungeon_light_level(SceneLightLevel.PITCH_DARK)
+			
+			var tile_map: TileMap = self.get_node("../TileMap")
+
+			# Make the walls invisible to get a "spooky" effect.
+			tile_map.set_layer_modulate(1, Color("ffffff00")) # 'Background wall' layer
+			tile_map.set_layer_modulate(2, Color("ffffff00")) # 'Foreground wall' layer
+			)
 
 	# Turn on the scene's "light" after exiting the third part of the maze.
 	self.get_node("../Props/TeleportersCollection/Teleporter5") \
 		.used \
-		.connect(func(): _set_dungeon_light_level(SceneLightLevel.NORMAL))
+		.connect(func(): 
+			_set_dungeon_light_level(SceneLightLevel.NORMAL)
+			
+			var tile_map: TileMap = self.get_node("../TileMap")
+
+			# Make the walls visible again.
+			tile_map.set_layer_modulate(1, Color("ffffffff")) # 'Background wall' layer
+			tile_map.set_layer_modulate(2, Color("ffffffff")) # 'Foreground wall' layer
+			)
 
 
 func _set_dungeon_light_level(level: SceneLightLevel) -> void:
