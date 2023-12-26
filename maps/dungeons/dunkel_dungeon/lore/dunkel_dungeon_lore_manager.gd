33d01586c9ceb1c7ee3ae2fcb6cd268c59a1a07d
Epic Man <124202346+cyan-wolf@users.noreply.github.com>
2023-12-26 13:57:17 -0400

    Added first "damaging light" area to Dunkel

--- maps/dungeons/dunkel_dungeon/lore/dunkel_dungeon_lore_manager.gd
+++ maps/dungeons/dunkel_dungeon/lore/dunkel_dungeon_lore_manager.gd
@@ -7,7 +7,20 @@ enum SceneLightLevel {
 	PITCH_DARK = 3,		# completely dark
 }
 
+# Causes the "darkness" effect in the scene.
 @onready var _darkness_modulate: CanvasModulate = self.get_node("../DarknessModulate")
+
+## Duration of the damaging light being on.
+@export var _damaging_light_on_duration: float = 2.0
+
+## Duration of the damaging light being off.
+@export var _damaging_light_off_duration: float = 2.0
+
+## Damage that the player receives per second.
+@export var _damaging_light_dps: float = 1.0
+
+var _damaging_light_is_active := false
+var _player_should_be_taking_damaging_light_damage := false
 
 func _ready():
 	# Dim the scene's "light" in the second part of the maze.
@@ -41,6 +54,30 @@ func _ready():
 			tile_map.set_layer_modulate(2, Color("ffffffff")) # 'Foreground wall' layer
 			)
 
+	self.get_node("../Props/TeleportersCollection/Teleporter5") \
+		.used \
+		.connect(_async_turn_on_line_of_sight_damaging_light)
+
+	# Setup timer for the damaging light's damage.
+	self.get_node("PlayerDamagingLightDamageTimer").wait_time = 1.0 / _damaging_light_dps
+
+	# Used to deal damage to the player in the "Line of sight" areas.
+	self.get_node("PlayerDamagingLightDamageTimer") \
+		.timeout \
+		.connect(_deal_player_damaging_light_damage)
+
+
+func _physics_process(_delta: float) -> void:
+	_player_should_be_taking_damaging_light_damage = false
+
+	for n in self.get_node("LineOfSight/Hitboxes").get_children():
+		var hitbox: Area2D = n
+
+		for other_hitbox in hitbox.get_overlapping_areas():
+			if other_hitbox.is_in_group("player_hitbox"):
+				_player_should_be_taking_damaging_light_damage = true
+				return # no need to keep checking
+
 
 func _set_dungeon_light_level(level: SceneLightLevel) -> void:
 	match level:
@@ -55,4 +92,49 @@ func _set_dungeon_light_level(level: SceneLightLevel) -> void:
 
 		SceneLightLevel.PITCH_DARK:
 			_darkness_modulate.color = Color("000000")
+
+
+func _async_turn_on_line_of_sight_damaging_light() -> void:
+	_damaging_light_is_active = true
+	
+	# Make the damaging lights visible.
+	for light in self.get_node("LineOfSight/Lights").get_children():
+		light.show()
+
+	# Enable the damaging light hitboxes.
+	for hitbox in self.get_node("LineOfSight/Hitboxes").get_children():
+		hitbox.get_node("CollisionPolygon2D").set_deferred("disabled", false)
+
+	# Make the eye sprites turn red.
+	for sprite in self.get_node("LineOfSight/EyeSprites").get_children():
+		sprite.play("attack")
+
+	await SceneManager.async_delay(_damaging_light_on_duration)
+
+	_async_turn_off_line_of_sight_damaging_light() # async call
+
+
+func _async_turn_off_line_of_sight_damaging_light() -> void:
+	_damaging_light_is_active = false
+	
+	# Make the damaging lights no longer visible.
+	for light in self.get_node("LineOfSight/Lights").get_children():
+		light.hide()
+
+	# Disable the damaging light hitboxes.
+	for hitbox in self.get_node("LineOfSight/Hitboxes").get_children():
+		hitbox.get_node("CollisionPolygon2D").set_deferred("disabled", true)
+
+	# Make the eye sprites no longer red.
+	for sprite in self.get_node("LineOfSight/EyeSprites").get_children():
+		sprite.play("idle")
+
+	await SceneManager.async_delay(_damaging_light_off_duration)
+
+	_async_turn_on_line_of_sight_damaging_light() # async call
+
+
+func _deal_player_damaging_light_damage() -> void:
+	if _player_should_be_taking_damaging_light_damage:
+		SceneManager.find_player().health_component.take_damage(1)
 
